313d87d21d286debbc081de4df2dfa603f8a5798
alvarochiqui <chiquilloooo@gmail.com>
2022-01-14 23:54:08 +0100

    Update b44_Modulos.py

---
+++ b44_Modulos.py
@@ -0,0 +1,12 @@
+#PASO 1
+
+#Este es un módulo con funciones que saludan
+def saludar ():
+    print("Hola, te saludo desde la función saludar del módulo saludos")
+
+#PASO 2
+#seguimos trabajando para ver que más podemos plasmar en test.py
+
+class Saludo():
+    def __init__(self):
+        print("Hola, saludos desde el init de la clase Saludo")
