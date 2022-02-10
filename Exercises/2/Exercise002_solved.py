103f9ae7b1e6bd41e3f58683d823628799b62716
alvarochiqui <chiquilloooo@gmail.com>
2022-02-10 11:43:05 +0100

    Exercise 2

---
+++ Exercises/2/Exercise002_solved.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 10 11:34:04 2022
+
+@author: alvaro
+"""
+'''
+Subset and conquer
+Subsetting Python lists is a piece of cake.
+Take the code sample below, which creates a list x and then selects "b" from it.
+Remember that this is the second element, so it has index 1.
+You can also use negative indexing.
+
+Remember the areas list from before, containing both strings and floats?
+Its definition is already in the script.
+Can you add the correct code to do some Python subsetting?
+'''
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[1])
+
+# Print out last element from areas
+print(areas[-1])
+
+# Print out the area of the living room
+print(areas[5])
