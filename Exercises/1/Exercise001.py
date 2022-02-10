67c20c487391abe9db5d75aa62082a6468491faa
alvarochiqui <chiquilloooo@gmail.com>
2022-02-10 10:52:52 +0100

    Exercise 1

---
+++ Exercises/1/Exercise001.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 10 10:43:34 2022
+
+@author: alvaro
+"""
+'''
+Instead of creating a flat list containing strings and floats,
+representing the names and areas of the rooms in your house,
+you can create a list of lists.
+The script in the editor can already give you an idea.
+
+Example,
+Don't get confused here: "hallway" is a string,
+while hall is a variable that represents the float 11.25 you specified earlier.
+'''
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# house information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv]
+
+# Print out house
+
+
+# Print out the type of house
