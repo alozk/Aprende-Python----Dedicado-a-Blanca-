3bf58d0e5c42edd16ab094f8f1deab36520dd7ce
alvarochiqui <chiquilloooo@gmail.com>
2022-02-11 16:30:12 +0100

    Exercises update

---
+++ 3 - Exercises/Exercise 3/Exercise003.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 10 17:32:14 2022
+
+@author: alvaro
+"""
+'''
+Subset and calculate
+After you've extracted values from a list,
+you can use them to perform additional calculations.
+Take this example,
+where the second and fourth element of a list x are extracted.
+The strings that result are pasted together using the + operator:
+
+x = ["a", "b", "c", "d"]
+print(x[1] + x[3])
+'''
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+
+
+# Print the variable eat_sleep_area
