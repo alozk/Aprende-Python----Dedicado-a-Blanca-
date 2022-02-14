053afae149a8990581182efb65b3fca3e1bfc50d
alvarochiqui <chiquilloooo@gmail.com>
2022-02-14 11:00:12 +0100

    Exercises update

---
+++ 3 - Exercises/Exercise 5/Exercise005.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 10 18:20:14 2022
+
+@author: alvaro
+"""
+'''
+Slicing and dicing (2)
+
+my_list[begin:end]
+However, it's also possible not to specify these indexes.
+If you don't specify the begin index,
+Python figures out that you want to start your slice at the beginning of your list.
+If you don't specify the end index,
+the slice will go all the way to the last element of your list.
+'''
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Alternative slicing to create downstairs
+
+
+# Alternative slicing to create upstairs
