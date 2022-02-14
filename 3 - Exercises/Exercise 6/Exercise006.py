bbe6fbbae0a014902efdfeefc779d3d8bef347aa
alvarochiqui <chiquilloooo@gmail.com>
2022-02-14 11:16:53 +0100

    Exercises update

---
+++ 3 - Exercises/Exercise 6/Exercise006.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 14 11:09:41 2022
+
+@author: alvaro
+"""
+'''
+Replace list elements
+Replacing list elements is pretty easy.
+Simply subset the list and assign new values to the subset.
+You can select single elements or you can change entire list slices at once.
+
+Instructions
+Update the area of the bathroom area to be 10.50 square meters instead of 9.50.
+Make the areas list more trendy! Change "living room" to "chill zone".
+'''
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Correct the bathroom area
+
+
+# Change "living room" to "chill zone"
