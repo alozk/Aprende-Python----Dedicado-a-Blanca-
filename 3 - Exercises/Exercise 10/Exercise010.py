6513d8752976c9b384dee455ccc4121bb10e0fc2
alvarochiqui <chiquilloooo@gmail.com>
2022-02-15 16:30:28 +0100

    Exercises update

---
+++ 3 - Exercises/Exercise 10/Exercise010.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 14 20:35:58 2022
+
+@author: alvaro
+"""
+'''
+Multiple arguments
+See that sorted() takes three arguments: iterable, key and reverse.
+
+key=None means that if you don't specify the key argument, it will be None.
+reverse=False means that if you don't specify the reverse argument, it will be False.
+
+In this exercise, you'll only have to specify iterable and reverse, not key.
+The first input you pass to sorted() will be matched to the iterable argument,
+but what about the second input?
+To tell Python you want to specify reverse without changing anything about key, you can use =:
+
+sorted(___, reverse = ___)
+Two lists have been created for you in the editor.
+Can you paste them together and sort them in descending order?
+
+Note: For now, we can understand an iterable as being any collection of objects, e.g. a List.
+
+Instructions
+Use + to merge the contents of first and second into a new list: full.
+Call sorted() on full and specify the reverse argument to be True.
+Save the sorted list as full_sorted.
+Finish off by printing out full_sorted.
+'''
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+
+
+# Sort full in descending order: full_sorted
+
+
+# Print out full_sorted
