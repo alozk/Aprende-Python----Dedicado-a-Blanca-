a2d58da55874f6b407ae148d1e766e9d7c46c148
alvarochiqui <chiquilloooo@gmail.com>
2022-02-16 18:04:27 +0100

    Exercises update

---
+++ 3 - Exercises/Exercise 012/Exercise012.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 16 17:49:32 2022
+
+@author: alvaro
+"""
+'''
+List Methods
+Strings are not the only Python types that have methods associated with them.
+Lists, floats, integers and booleans are also types that come packaged with a bunch of useful methods.
+In this exercise, you'll be experimenting with:
+
+index(), to get the index of the first element of a list that matches its input and
+count(), to get the number of times an element appears in a list.
+You'll be working on the list with the area of different parts of a house: areas.
+
+Instructions
+Use the index() method to get the index of the element in areas that is equal to 20.0.
+Print out this index.
+Call count() on areas to find out how many times 9.50 appears in the list.
+Again, simply print out this number.
+'''
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Print out the index of the element 20.0
+
+
+# Print out how often 9.50 appears in areas
